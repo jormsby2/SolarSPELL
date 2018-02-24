b757772c5e3d4c3c11a258b9be9bf07e4b0e4931
Matthew Seiler <mdseiler@asu.edu>
2018-02-24 16:35:25 -0700

    fix source and dest paths

--- src/scripts/removeduplicates.py
+++ src/scripts/removeduplicates.py
@@ -1,5 +1,5 @@
-source_path = "/home/pi/access1.log.bak"
-dest_path = "/home/pi/access2.log"
+source_path = "/var/log/apache2/access1.log.bak"
+dest_path = "/var/log/apache2/access2.log"
 
 def remove_duplicate_lines(source_path):
 	uniq = set()
