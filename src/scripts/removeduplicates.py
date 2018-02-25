ac06922a9fbce1e81c9ff6afec9c9375870a6c38
Matthew Seiler <mdseiler@asu.edu>
2018-02-24 17:29:55 -0700

    add comment for sort -u command

--- src/scripts/removeduplicates.py
+++ src/scripts/removeduplicates.py
@@ -1,6 +1,7 @@
 source_path = "/var/log/apache2/access1.log.bak"
 dest_path = "/var/log/apache2/access2.log"
 
+#consider using sort -u command instead of this script
 def remove_duplicate_lines(source_path):
 	uniq = set()
 	with open(source_path, "r") as fin:
