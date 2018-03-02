7ec3aa98ba311e857bd09a03de03bd627d5c8664
Matthew Seiler <mdseiler@asu.edu>
2018-03-02 15:23:48 -0700

    download csv sqlite

--- src/usage/downloadcsv.php
+++ src/usage/downloadcsv.php
@@ -1,6 +1,15 @@
 <?php
+define("DB_PATH", "/var/www/db/UserData.db");
+
+class MyDB extends SQLite3
+{
+	function __construct() {
+		$this->open(DB_PATH);
+	}
+}
+
     if (isset($_POST['download'])){
-		$connectionstring = 'mysql:host=localhost;dbname=UserData';
+		/*$connectionstring = 'mysql:host=localhost;dbname=UserData';
 		$user = 'root';
 		$passwd = 'raspberry';
 
@@ -10,32 +19,40 @@
 			// print error message.
 			echo($e->getMessage());
 			die();
-		}
-		$fname = "usagedata-" . date('m-d-Y') . "csv";
+		}*/
+		$db = new MyDB();
+		$fname = "usagedata-" . date('m-d-Y') . ".csv";
 		$query = "SELECT * FROM UserLogInfo";
-		$statement = $dbc->prepare($query);
-		$statement->execute();
-		$result = $statement->fetchAll(PDO::FETCH_ASSOC);
-		
+		//$statement = $dbc->prepare($query);
+		$statement = $db->prepare($query);
+		//$statement->execute();
+		//$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+		$result = $statement->execute();
 		$columnNames = array();
+		$firstRow = [];
 		if(!empty($result)){
-			$firstRow = $result[0];
+			//$firstRow = $result[0];
+			$firstRow = $result->fetchArray(SQLITE3_ASSOC);
 			foreach($firstRow as $colName => $val){
 				$columnNames[] = $colName;
 			}
 		}
-		
+
         header('Content-type: text/csv');
         header('Content-Disposition: attachment; filename="' . $fname . '";');
         header('Pragma: no-cache');
         header('Expires: 0');
         $file = fopen('php://output', 'w');
 		fputcsv($file, $columnNames);
-		foreach($result as $row){
+		fputcsv($file, $firstRow);
+		/*foreach($result as $row){
+			fputcsv($file, $row);
+		}*/
+		while($row = $result->fetchArray(SQLITE3_ASSOC)) {
 			fputcsv($file, $row);
 		}
 		fclose($file);
-        
+
         exit();
     }
 ?>
