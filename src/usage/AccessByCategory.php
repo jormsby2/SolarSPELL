fd2bc2a74afc037c88fbf2f9d559871afe3d81b3
Matthew Seiler <mdseiler@asu.edu>
2018-03-02 13:19:45 -0700

    modify pie chart to use chart.js and sqlite

--- src/usage/AccessByCategory.php
+++ src/usage/AccessByCategory.php
@@ -2,61 +2,88 @@
 /*
  * This page formats the log data into a pi chart to show user access by category. I am currently using dummy data
  * until we figure out the database and logging.
-
-Below is an example of what getting the data will look like once we have the database and logging figured out
-$conn = mysqli_connect(connection to my sql db);
-$query = "SELECT category, count(*) as number FROM tbl_logs GROUP BY category";
-$result = mysqli_query($conn, $query);
 */
+//Below is an example of what getting the data will look like once we have the database and logging figured out
+//$conn = mysqli_connect(connection to my sql db);
+define("DB_PATH", "/var/www/db/UserData.db");
+$query = "SELECT main_category, count(*) as number FROM UserLogInfo GROUP BY main_category";
+//$result = mysqli_query($conn, $query);
+
 // dummy data to work on this task while we figure out our logging issues
+class MyDB extends SQLite3
+{
+	function __construct() {
+		$this->open(DB_PATH);
+	}
+}
+
+$db = new MyDB();
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>Pi Chart of User Access</title>
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-        google.charts.load('current', {'packages':['corechart']});
-        google.charts.setOnLoadCallback(drawChart);
-        function drawChart()
-        {
-            // below is commented out until we have a proper result from the mysql database
-            //var data = google.visualization.arrayToDataTable([
-           // ['Category', 'Number'],
-            <?php
-            /*
-            while($row = mysqli_fetch_array($result))
-            {
-                echo "['".$row["category"]."', ".$row["number"]."],";
-            }
-            */
-            ?>
-            //]);
-            var data = google.visualization.arrayToDataTable([
-                ['Category', 'Times Accessed'],
-                ['Environment', 110],
-                ['Health and Safety', 12],
-                ['Language Arts', 17],
-                ['Local Topics', 24],
-                ['Math', 35],
-                ['Science', 24]
-            ]);
-
-        var options = {
-            title: 'Percentage of Access to Each Category',
-            pieHole: 0.1
-        };
-        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-        chart.draw(data, options);
-        }
-    </script>
 </head>
 <body>
 <br /><br />
 <div style="width:900px;">
     <h3 align="center">Pi Chart of User Access</h3>
     <br />
-    <div id="piechart" style="width: 900px; height: 500px;"></div>
+    <div id="piechart-container" style="width: 900px; height: 500px;">
+        <canvas id="piechart" style="width: 100%; height: 100%;"></canvas>
+    </div>
 </div>
+<script type="text/javascript" src="Chart.min.js"></script>
+<script type="text/javascript">
+	function getData() {
+		return([
+			<?php
+			$result = $db->query($query);
+			$row = $result->fetchArray();
+
+			while($row != FALSE) {
+				echo "['" . $row["main_category"] . "', " . $row["number"] . "],";
+				$row = $result->fetchArray();
+			}
+			?>
+		]);
+	}
+
+	function getRand(min, max) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
+	function drawChart(elem_id, data) {
+		var bgcs = [];
+		var length = data.length;
+		var categories = data.map(function(x) { return x[0] });
+		var counts = data.map(function(x) { return x[1] });
+
+		for (var i = 0; i < length; i++) {
+			bgcs.push('rgb(' + getRand(0,255) + ',' + getRand(0, 255) + ',' + getRand(0,255) +  ')');
+		}
+
+		var data = {
+			datasets: [{
+				backgroundColor: bgcs,
+				data: counts
+			}],
+
+			labels: categories
+		};
+
+		var options = {};
+		var ctx = document.getElementById(elem_id).getContext('2d');
+		var chart= new Chart(ctx, {
+			type: 'pie',
+			data: data,
+			options: options
+		});
+	}
+
+	drawChart('piechart', getData());
+</script>
 </body>
 </html>
