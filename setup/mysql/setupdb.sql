c28bd392be21b07717a247c621d3960a44429ea5
ajbowen1 <ajbowen1@asu.edu>
2018-04-15 14:35:52 -0400

    Removed unnecessary & unused dir for mysql

--- setup/mysql/setupdb.sql
+++
@@ -1,3 +0,0 @@
-use UserData
-create table if not exists UserLogInfo (filename VARCHAR(120), useragent VARCHAR(120));
-load data local infile '/var/log/apache2/testdbinfo.txt' into table UserLogInfo;
