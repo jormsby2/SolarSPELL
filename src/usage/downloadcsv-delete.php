c3a4e3e88a6b4cf7616787a427a67194c60ef162
Matthew Seiler <mdseiler@asu.edu>
2018-04-18 12:16:16 -0700

    get date from client

--- src/usage/downloadcsv-delete.php
+++ src/usage/downloadcsv-delete.php
@@ -9,42 +9,48 @@ class MyDB extends SQLite3
 }
 
     if (isset($_POST['delete'])){
-		$db = new MyDB();
-		$fname = "usagedata-" . date('m-d-Y') . ".csv";
-		$query = "SELECT * FROM UserLogInfo";
-		
-		$statement = $db->prepare($query);
-		
-		$result = $statement->execute();
-		$columnNames = array();
-		$firstRow = [];
-		if(!empty($result)){
-			
-			$firstRow = $result->fetchArray(SQLITE3_ASSOC);
-			foreach($firstRow as $colName => $val){
-				$columnNames[] = $colName;
-			}
-            $deleteStatement = "delete from UserLogInfo";
-            $db->exec($deleteStatement);
-	    unlink(DB_PATH);
-	    $newDB = new MyDB();
-	    $newDB->exec('create table UserLogInfo (main_category VARCHAR(120), file_name VARCHAR(120), file_path VARCHAR(120), browser VARCHAR(120), device_type VARCHAR(120), os VARCHAR(120))'); 
-		}
+      $db = new MyDB();
 
-        header('Content-type: text/csv');
-        header('Content-Disposition: attachment; filename="' . $fname . '";');
-        header('Pragma: no-cache');
-        header('Expires: 0');
-        $file = fopen('php://output', 'w');
-		fputcsv($file, $columnNames);
-		fputcsv($file, $firstRow);
-		
-		while($row = $result->fetchArray(SQLITE3_ASSOC)) {
-			fputcsv($file, $row);
-		}
-		fclose($file);
-        
+      if (isset($_POST['userdate'])) {
+        $currdate = $_POST['userdate'];
+      } else {
+        $currdate = date('m-d-Y');
+      }
 
-        exit();
+      $fname = "usagedata-" . $currdate . ".csv";
+      $query = "SELECT * FROM UserLogInfo";
+
+      $statement = $db->prepare($query);
+
+      $result = $statement->execute();
+      $columnNames = array();
+      $firstRow = [];
+      if(!empty($result)){
+
+        $firstRow = $result->fetchArray(SQLITE3_ASSOC);
+        foreach($firstRow as $colName => $val){
+          $columnNames[] = $colName;
+        }
+              $deleteStatement = "delete from UserLogInfo";
+              $db->exec($deleteStatement);
+        unlink(DB_PATH);
+        $newDB = new MyDB();
+        $newDB->exec('create table UserLogInfo (main_category VARCHAR(120), file_name VARCHAR(120), file_path VARCHAR(120), browser VARCHAR(120), device_type VARCHAR(120), os VARCHAR(120))');
+      }
+
+      header('Content-type: text/csv');
+      header('Content-Disposition: attachment; filename="' . $fname . '";');
+      header('Pragma: no-cache');
+      header('Expires: 0');
+      $file = fopen('php://output', 'w');
+      fputcsv($file, $columnNames);
+      fputcsv($file, $firstRow);
+
+      while($row = $result->fetchArray(SQLITE3_ASSOC)) {
+        fputcsv($file, $row);
+      }
+      fclose($file);
+
+      exit();
     }
 ?>
--- src/usage/downloadcsv-keep.php
+++ src/usage/downloadcsv-keep.php
@@ -9,36 +9,42 @@ class MyDB extends SQLite3
 }
 
     if (isset($_POST['keep'])){
-		$db = new MyDB();
-		$fname = "usagedata-" . date('m-d-Y') . ".csv";
-		$query = "SELECT * FROM UserLogInfo";
-		
-		$statement = $db->prepare($query);
-		
-		$result = $statement->execute();
-		$columnNames = array();
-		$firstRow = [];
-		if(!empty($result)){
-			
-			$firstRow = $result->fetchArray(SQLITE3_ASSOC);
-			foreach($firstRow as $colName => $val){
-				$columnNames[] = $colName;
-			}
-		}
+      $db = new MyDB();
 
-        header('Content-type: text/csv');
-        header('Content-Disposition: attachment; filename="' . $fname . '";');
-        header('Pragma: no-cache');
-        header('Expires: 0');
-        $file = fopen('php://output', 'w');
-		fputcsv($file, $columnNames);
-		fputcsv($file, $firstRow);
-		
-		while($row = $result->fetchArray(SQLITE3_ASSOC)) {
-			fputcsv($file, $row);
-		}
-		fclose($file);
+      if (isset($_POST['userdate'])) {
+        $currdate = $_POST['userdate'];
+      } else {
+        $currdate = date('m-d-Y');
+      }
 
-        exit();
+      $fname = "usagedata-" . $currdate . ".csv";
+      $query = "SELECT * FROM UserLogInfo";
+
+      $statement = $db->prepare($query);
+
+      $result = $statement->execute();
+      $columnNames = array();
+      $firstRow = [];
+      if(!empty($result)){
+        $firstRow = $result->fetchArray(SQLITE3_ASSOC);
+        foreach($firstRow as $colName => $val){
+          $columnNames[] = $colName;
+        }
+      }
+
+      header('Content-type: text/csv');
+      header('Content-Disposition: attachment; filename="' . $fname . '";');
+      header('Pragma: no-cache');
+      header('Expires: 0');
+      $file = fopen('php://output', 'w');
+      fputcsv($file, $columnNames);
+      fputcsv($file, $firstRow);
+
+      while($row = $result->fetchArray(SQLITE3_ASSOC)) {
+        fputcsv($file, $row);
+      }
+      fclose($file);
+
+      exit();
     }
 ?>
